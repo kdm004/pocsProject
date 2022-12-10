eaec31fef75f4c5e068c37794675767b4521f118
Kevin Motia <kevindmotia@gmail.com>
2022-12-09 21:59:27 -0500

    prelim2 tweet by region sorter

--- xxgeoparser.py
+++ xxgeoparser.py
@@ -11,16 +11,36 @@ from geotext import GeoText
 # GeoText('New York, Texas, and also China').country_mentions
 # # OrderedDict([(u'US', 2), (u'CN', 1)])
 
-sentences_with_MENA = []
-tweet_list=['my friend is from a middle eastern country', 'hello my European friend', 'Hey, meet my friend from Laos']
+# tweets_with_mentions = []
+# tweet_list=['my friend is from a middle eastern country', 'hello my European friend', 'Hey, meet my friend from Laos', 'hello my South Asian friend!', 'I am from Vermont']
+
+
+# for tweet in tweet_list:                                                # for each tweet
+#     places = GeoText(tweet, aggressive = True).country_mentions         # get the regions mentioned
+#     places_list = list(places.keys())                                   # make a list of the regions mentioned
+#     print(places_list)                                                  # 
+#     if 'Europe' or 'Latin America' or 'North America' or 'Indian Subcontinent' or 'Sub-Saharan Africa' or 'MENA' or 'East Asia' or 'Central Asia' or 'Pacific' in places_list:
+#         tweets_with_mentions.append(tweet)
+# print(tweets_with_mentions)
+
+tweets_with_mentions = []
+tweet_list = ['hello', 'I am iranian', 'Are you from China?', 'Whose dog is that?']
 for tweet in tweet_list:
-    places = GeoText(tweet, aggressive = True).country_mentions
-    places_list = list(places.keys())
- #   print(places_list)
-    if 'MENA' in places_list:
-        sentences_with_MENA.append(tweet)
-print(sentences_with_MENA)
+    places = GeoText(tweet, aggressive = True).country_mentions         # get the regions mentioned
+    region_list = list(places.keys())
+#    print(region_list)
+    for entry in region_list:
+        if 'MENA' in entry:
+            print('yes')
+            tweets_with_mentions.append(tweet)
+print(tweets_with_mentions)
+
+
+
+
+
 
 # It's not perfect, but this is what we're going to use to filter tweets by country and region.
 # We're going to need to find out how to add "region" into it. Maybe we'll do that if we have time.
 # We're going to need to find out how to change the output from <country_abbrev> to <country_region>
+
